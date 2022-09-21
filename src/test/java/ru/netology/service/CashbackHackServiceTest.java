400168469ab061b4446f243e7de7de85c51de715
VikaMin <4673813@gmail.com>
2022-09-22 00:36:59 +0300

    x

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 class CashbackHackServiceTest {
 
@@ -14,6 +14,6 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
